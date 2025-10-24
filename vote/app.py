96b58458ac0e729301ce63992ed29089bd6a75d2
naufaleka <opaleka16@gmail.com>
2025-10-24 10:36:54 +0700

    Perubahan option

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Chocolate")
-option_b = os.getenv('OPTION_B', "Vanilla")
+option_a = os.getenv('OPTION_A', "CAT")
+option_b = os.getenv('OPTION_B', "DOG")
 hostname = socket.gethostname()
 version = 'v1'
 
